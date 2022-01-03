14159b2e13fb077fd8449116cab45e442bf2ad77
ingramirez <ing.ramirezrivera@gmail.com>
2022-01-03 11:46:43 -0500

    Funcion con ciclo for para calcular el promedio

--- promedio.js
+++ promedio.js
@@ -1,23 +1,25 @@
+ /*const lista1 = [
+     100,
+     200,
+     300,
+     500
 
-//-Primer paso: definir formulas
-//-Segundo paso: implementar las formulas en Javascript
-//-Tercer paso: crear funciones
-//-Cuarto paso: integrar JS con HTML
+ ];
 
-
-//calcular promedio
-
-const lista1 = [100,200,300,500];
 let sumaLista1 = 0;
-
-
-
-for (let i = 0; i < lista1.length ; i++ ) {
-     sumaLista1 = sumaLista1 + lista1[i];
+for(let i = 0; i < lista1.length; i++){
+    sumaLista1 = sumaLista1 + lista1[i];
 }
 
-const promedioLista1 = sumaLista1 / lista1.length;
-console.log(sumaLista1);
-console.log(lista1.length);
-console.log(promedioLista1);
+const promedio = sumaLista1 / lista1.length;
+*/
 
+function calcularMediaAritemtica(lista){
+    let sumaLista = 0;
+    for(let i = 0; i < lista.length; i++){
+    sumaLista = sumaLista + lista[i];
+    }
+
+    const promedio = sumaLista / lista.length;
+    return promedio;
+}
