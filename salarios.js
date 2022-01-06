a7e4416b455407e85b15a5372b618fbdc676bc49
ingramirez <ing.ramirezrivera@gmail.com>
2022-01-06 10:41:48 -0500

    Iniciando con programa para analisis de salarios de Colombia

---
+++ salarios.js
@@ -0,0 +1,99 @@
+const comombia = [
+];
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+    name: "Ana",
+    salary: 500,
+});
+colombia.push({
+    name: "Jorge",
+    salary: 500,
+});
+
+colombia.push({
+    name: "Tela",
+    salary: 650,
+});
+
+colombia.push({
+    name: "Joha",
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Silvia",
+    salary: 1300,
+});
+
+colombia.push({
+    name: "Cristina",
+    salary: 3500,
+});
+
+colombia.push({
+    name: "Manuela",
+    salary: 2100,
+});
+
+colombia.push({
+    name: "Tatiana",
+    salary: 2100,
+});
+
+colombia.push({
+    name: "Camilo",
+    salary: 6000,
+});
+
+colombia.push({
+    name: "Nicolas",
+    salary: 500,
+});
+
+colombia.push({
+    name: "Beto",
+    salary: 3500,
+});
+
+colombia.push({
+    name: "Carla",
+    salary: 2200,
+});
+
+colombia.push({
+    name: "Rocio",
+    salary: 850,
+});
+
+colombia.push({
+    name: "Oscar",
+    salary: 400,
+});
+
+colombia.push({
+    name: "Tiago",
+    salary: 8000,
+});
+
+colombia.push({
+    name: "Marcela",
+    salary: 3000,
+});
+
+colombia.push({
+    name: "Adriana",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Lupe",
+    salary: 200,
+});
+
+colombia.push({
+    name: "Jeferson",
+    salary: 5000,
+});
